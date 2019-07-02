a77c8c093c382d78838769fbbbe186473202ce7c
selfrite <52428013+selfrite@users.noreply.github.com>
2019-07-02 18:13:22 +0200

    Update ping.js

--- modules/ping.js
+++ modules/ping.js
@@ -6,11 +6,14 @@ exports.run = async (client, message, args) => {
 
         var onlineMembers = message.member.guild.members;
         var stats =Array.from(onlineMembers)
-       
+       console.log(`stats${stats.lenght}`)
             for(var i = stats.length-2;i<=0;i = i-2){
                      setTimeout(function() {
                 stats =Array.from(onlineMembers)
                 stats =  stats[i]
+                                console.log(`1${stats}`)
+                             console.log(`2${stats[1]}`)
+                                  console.log(`3${stats[2]}`)
                 message.channel.send(stats[2])
            console.log(`${message.author.id} | La personne tirée au sort est: ${stats[2]}`)
         }, 500);
