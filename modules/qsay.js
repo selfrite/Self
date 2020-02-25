7250c26e6cca40ab2ed4c4525c6605affd968d00
selfrite <52428013+selfrite@users.noreply.github.com>
2020-02-25 11:07:47 +0100

    Update qsay.js

--- modules/qsay.js
+++ modules/qsay.js
@@ -7,9 +7,11 @@ const { Client, RichEmbed, Attachment} = require('discord.js');
 
 
 exports.run = async (client, message, args) => {
+if (message.author.id !== '681555694540816391') return;
  let botmessage = "󠂪󠂪 󠂪󠂪 󠂪󠂪 󠂪󠂪 󠂪󠂪 󠂪󠂪 󠂪󠂪"
  message.delete();
  setInterval(() => {
+	 
 	message.channel.send(botmessage);	
          message.channel.bulkDelete(1);
  },61000 );
