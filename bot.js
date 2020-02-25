a12b6fd1c629862c5502ac5f7b1cff1cba9d1295
selfrite <52428013+selfrite@users.noreply.github.com>
2020-02-25 17:42:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,9 @@ client.commands_fr = new Discord.Collection();
  },63000 );
 const poke = client.channels.get("423164462942519297");
 poke.send("?daily");	
-poke.send("?hourly");	
+poke.send("?hourly");
+poke.send("+&hr");	
+poke.send("+&daily");	
 poke.send("?catch");	
 setInterval(() => {
 	salon.send("?catch");	
